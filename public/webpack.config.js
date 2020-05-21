f488bd78126e332214b4e2bc34577ecfaa973569
Lauren Doss <doss.laurene@gmail.com>
2020-05-21 11:32:16 -0700

    updated to include webpack, db.js, and server-worker.js

---
+++ public/webpack.config.js
@@ -0,0 +1,11 @@
+const config = {
+    entry: "index.js",
+    output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+    },
+    mode: "development"
+  };
+  
+  module.exports = config;
+  
